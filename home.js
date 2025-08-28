3e963fb243a2384b90c5dd00678b8e15ba54c3ba
arpita-devcode <arpitadebnath940@gmail.com>
2025-08-28 17:23:17 +0600

    added a new code

--- home.js
+++ home.js
@@ -1,25 +1,94 @@
 document
   .getElementById("button_clicked")
   .addEventListener("click", function () {
-    const bank = document.getElementById("bank").value;
-    const accountNumber = document.getElementById("bank-number").value;
-    const pinNumber = document.getElementById("add-pin-number").value;
-    const addAmount = parseInt(document.getElementById("Add-amount").value);
+    const bank = getinputvalue("bank");
+    const accountNumber = getinputvalue("bank-number");
+    const pinNumber = getinputvalue("add-pin-number");
+    const addAmount = getinputbyid("Add-amount");
     const valid_pin = 1234;
-    const addamountNumber = parseInt(document.getElementById("Add-number").innerText);
-    if(accountNumber.length<11){
+    const addamountNumber = getinputinnertext("Add-number");
+    if (accountNumber.length < 11) {
       alert("please provide valid account number");
       return;
     }
 
-    if(pinNumber!=valid_pin){
+    if (pinNumber != valid_pin) {
       alert("please provide valid pin");
     }
-    
-    if(addAmount<0){
-      alert("you have to add postive number")
+    if (addAmount < 0) {
+      alert("you have to add postive number");
     }
     const in_total = addAmount + addamountNumber;
-    console.log(in_total);
-   document.getElementById("Add-number").innerText = in_total;
+    // console.log(in_total);
+   setInnerText(in_total);
   });
+
+//  cashout button work
+document.getElementById("withdraw_clicked").addEventListener("click",function(){
+  const minusvalue= getinputinnertext("Add-number");
+  const cashout = getinputbyid('Add-cashoutamount');
+  const pinNUMBER = getinputvalue("add-pin-number1")
+  if(pinNUMBER.length != 4){
+    alert("aha try agin with right pin")
+    return ;
+  }
+  const cashoutMoney = minusvalue-cashout;
+  setInnerText(cashoutMoney);
+})
+  // set innertext as availabe balance
+  function setInnerText(value){
+    console.log(value);
+    const balance= document.getElementById("Add-number");
+   balance.innerText = value;
+  }
+//  function to covert input values in number 
+function getinputbyid(id){
+  const inputID = document.getElementById(id);
+  const inputvalue = inputID.value;
+  const getinputvaluenumber = parseInt(inputvalue);
+  return  getinputvaluenumber;}
+
+function getinputvalue(id){
+  const inputId = document.getElementById(id);
+  const indputget = inputId.value;
+  return indputget;
+}
+function getinputinnertext(id){
+  const inputId = document.getElementById(id);
+  const indputget1 = inputId.innerText;
+  const convert = parseInt(indputget1)
+  return convert;
+}
+// toggle feature
+document.getElementById("add-Money").addEventListener("click", function () {
+  handtoggle("paymentParent")
+});
+document.getElementById("cash-Out").addEventListener("click", function () {
+ 
+  handtoggle("cashoutParent")
+})
+document.getElementById("traniction-button").addEventListener("click", function () {
+
+ handtoggle("Transaction")
+})
+document.getElementById("bonus").addEventListener("click", function () {
+
+ handtoggle("GetBonus")
+})
+document.getElementById("billPayment").addEventListener("click", function () {
+
+ handtoggle("bill")
+})
+// document.getElementById("").addEventListener("click", function () {
+
+//  handtoggle("GetBonus")
+// })
+
+// function
+function handtoggle(id){
+  const forms = document.getElementsByClassName("form");
+    for(const form of forms){
+    form.style.display = 'none';
+  }
+   document.getElementById(id).style.display = "block";
+}

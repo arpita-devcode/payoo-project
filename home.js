4d7607989a2e3b2c3b695b380ec8c5863caee828
arpita-devcode <arpitadebnath940@gmail.com>
2025-08-28 13:03:04 +0600

    added a new js file

--- home.js
+++ home.js
@@ -5,7 +5,20 @@ document
     const accountNumber = document.getElementById("bank-number").value;
     const pinNumber = document.getElementById("add-pin-number").value;
     const addAmount = parseInt(document.getElementById("Add-amount").value);
+    const valid_pin = 1234;
     const addamountNumber = parseInt(document.getElementById("Add-number").innerText);
+    if(accountNumber.length<11){
+      alert("please provide valid account number");
+      return;
+    }
+
+    if(pinNumber!=valid_pin){
+      alert("please provide valid pin");
+    }
+    
+    if(addAmount<0){
+      alert("you have to add postive number")
+    }
     const in_total = addAmount + addamountNumber;
     console.log(in_total);
    document.getElementById("Add-number").innerText = in_total;

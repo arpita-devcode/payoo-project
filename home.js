5336ac13a9c9c64f615c13ba695d34a19bcac21e
arpita-devcode <arpitadebnath940@gmail.com>
2025-08-28 23:41:20 +0600

    added a new filr

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+const transaction = [];
 document
   .getElementById("button_clicked")
   .addEventListener("click", function () {
@@ -20,75 +21,149 @@ document
     }
     const in_total = addAmount + addamountNumber;
     // console.log(in_total);
-   setInnerText(in_total);
+    setInnerText(in_total);
+    const data = {
+      name: "Add Money",
+      date: new Date().toLocaleString(),
+    };
+    transaction.push(data);
+    // console.log(transaction);
   });
 
 //  cashout button work
-document.getElementById("withdraw_clicked").addEventListener("click",function(){
-  const minusvalue= getinputinnertext("Add-number");
-  const cashout = getinputbyid('Add-cashoutamount');
-  const pinNUMBER = getinputvalue("add-pin-number1")
-  if(pinNUMBER.length != 4){
-    alert("aha try agin with right pin")
-    return ;
-  }
-  const cashoutMoney = minusvalue-cashout;
-  setInnerText(cashoutMoney);
-})
-  // set innertext as availabe balance
-  function setInnerText(value){
-    console.log(value);
-    const balance= document.getElementById("Add-number");
-   balance.innerText = value;
-  }
-//  function to covert input values in number 
-function getinputbyid(id){
+document
+  .getElementById("withdraw_clicked")
+  .addEventListener("click", function () {
+    const minusvalue = getinputinnertext("Add-number");
+    const cashout = getinputbyid("Add-cashoutamount");
+    const pinNUMBER = getinputvalue("add-pin-number1");
+    if (pinNUMBER.length != 4) {
+      alert("aha try agin with right pin");
+      return;
+    }
+    if (cashout <= 0) {
+      alert("You must withdraw a positive amount");
+      return;
+    }
+    
+    const cashoutMoney = minusvalue - cashout;
+    if (cashoutMoney < 0) { 
+      alert("You don't have enough money");
+      return;
+    }
+    setInnerText(cashoutMoney);
+    const data = {
+      name: "Cashout Money",
+      date: new Date().toLocaleString(),
+    };
+    transaction.push(data);
+    // console.log(transaction);
+  });
+
+document
+  .getElementById("Transactionhistory")
+  .addEventListener("click", function () {
+    const trasiction_history = document.getElementById("transaction-container");
+    trasiction_history.innerText = "";
+    for (const data of transaction) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+         <div class="max-w-lg mx-auto" id="transaction-container">
+            <div class="flex justify-between p-2 mt-2 ">
+                <h1 class="font-bold text-xl">Latest payment</h1>
+                <p>View All</p>
+            </div>
+            <div class="bg-white p-4 rounded-xl">
+                <div class="flex justify-between ">
+                    <div class="flex items-center gap-2">
+                        <div class="">
+                            <img src="assests/wallet1.png" alt=""
+                                class="border-1 rounded-full p-5 bg-[#f4f5f7] border-none">
+                        </div>
+                        <div>
+                            <h1 class="font-bold">${data.name}</h1>
+                            <p class="mt-1">${data.date}</p>
+                        </div>
+                    </div>
+                    <div><i class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+            </div>
+        </div>
+        `;
+      trasiction_history.appendChild(div);
+    }
+  });
+// set innertext as availabe balance
+function setInnerText(value) {
+  console.log(value);
+  const balance = document.getElementById("Add-number");
+  balance.innerText = value;
+}
+//  function to covert input values in number
+function getinputbyid(id) {
   const inputID = document.getElementById(id);
   const inputvalue = inputID.value;
   const getinputvaluenumber = parseInt(inputvalue);
-  return  getinputvaluenumber;}
+  return getinputvaluenumber;
+}
 
-function getinputvalue(id){
+function getinputvalue(id) {
   const inputId = document.getElementById(id);
   const indputget = inputId.value;
   return indputget;
 }
-function getinputinnertext(id){
+function getinputinnertext(id) {
   const inputId = document.getElementById(id);
   const indputget1 = inputId.innerText;
-  const convert = parseInt(indputget1)
+  const convert = parseInt(indputget1);
   return convert;
 }
 // toggle feature
 document.getElementById("add-Money").addEventListener("click", function () {
-  handtoggle("paymentParent")
+  handtoggle("paymentParent");
+  selectColour("add-Money");
 });
 document.getElementById("cash-Out").addEventListener("click", function () {
- 
-  handtoggle("cashoutParent")
-})
-document.getElementById("traniction-button").addEventListener("click", function () {
-
- handtoggle("Transaction")
-})
+  selectColour("cash-Out");
+  handtoggle("cashoutParent");
+});
+document
+  .getElementById("traniction-button")
+  .addEventListener("click", function () {
+    selectColour("traniction-button");
+    handtoggle("Transaction");
+  });
 document.getElementById("bonus").addEventListener("click", function () {
-
- handtoggle("GetBonus")
-})
+  selectColour("bonus");
+  handtoggle("GetBonus");
+});
 document.getElementById("billPayment").addEventListener("click", function () {
-
- handtoggle("bill")
-})
-// document.getElementById("").addEventListener("click", function () {
-
-//  handtoggle("GetBonus")
-// })
+  selectColour("billPayment");
+  handtoggle("bill");
+});
+document
+  .getElementById("Transactionhistory")
+  .addEventListener("click", function () {
+    selectColour("Transactionhistory");
+    handtoggle("history");
+  });
 
 // function
-function handtoggle(id){
+function handtoggle(id) {
   const forms = document.getElementsByClassName("form");
-    for(const form of forms){
-    form.style.display = 'none';
+  for (const form of forms) {
+    form.style.display = "none";
   }
-   document.getElementById(id).style.display = "block";
+  document.getElementById(id).style.display = "block";
+}
+function selectColour(id) {
+  const button = document.getElementsByClassName("form-btn");
+  for (buttons of button) {
+    buttons.classList.remove("border-[#0874F2]", "bg-[#0874F20d]");
+    buttons.classList.add("border-gray-300");
+  }
+  document.getElementById(id).classList.remove("border-gray-300");
+  document
+    .getElementById(id)
+    .classList.add("border-[#0874F2]", "bg-[#0874F20d]");
 }
